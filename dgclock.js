48a2e046261b610e4016b1bbd6863882edef37d5
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2024-01-06 22:15:34 +0500

    modified Js code

--- dgclock.js
+++ dgclock.js
@@ -1,13 +1,6 @@
-let hrs = document.getElementById('hrs')
-let mins = document.getElementById('mins')
-let sec = document.getElementById('sec')
-let currentTime = new Date()
-currentTime.toDateString()
+let clock = document.querySelector('.clock')
 
-setInterval (function(){
-    let currentTime = new Date()
-    currentTime.toDateString() 
-    hrs.innerHTML = ((currentTime.getHours()<10)?  '0':'' )+ currentTime.getHours()
-    mins.innerHTML = ((currentTime.getMinutes()<10)? '0':'')+currentTime.getMinutes()
-    sec.innerHTML = ((currentTime.getSeconds()<10)? '0':'')+currentTime.getSeconds()
+setInterval(function(){
+    let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
 },1000)
